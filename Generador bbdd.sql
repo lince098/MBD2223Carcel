4a6d65d21ed4f0cadb3233962dfc27d980e2fd06
Cristian Torres <cristiantorr98@gmail.com>
2022-12-08 10:59:03 +0100

    Generador bbdd actualizado

--- Generador bbdd.sql
+++ Generador bbdd.sql
@@ -1,5 +1,5 @@
 -- Generado por Oracle SQL Developer Data Modeler 22.2.0.165.1149
---   en:        2022-11-29 19:22:45 CET
+--   en:        2022-12-08 10:50:08 CET
 --   sitio:      Oracle Database 11g
 --   tipo:      Oracle Database 11g
 
@@ -35,48 +35,68 @@ CREATE TABLE cocinero (
 ALTER TABLE cocinero ADD CONSTRAINT cocinero_pk PRIMARY KEY ( nif );
 
 CREATE TABLE compra (
-    reclusos_nif         VARCHAR2(9) NOT NULL,
+    recluso_nif          VARCHAR2(9) NOT NULL,
     producto_id_producto INTEGER NOT NULL,
-    fecha_de_compra      DATE NOT NULL
+    fecha_de_compra      DATE NOT NULL,
+    cantidad             INTEGER NOT NULL
 );
 
-ALTER TABLE compra ADD CONSTRAINT compra_pk PRIMARY KEY ( reclusos_nif,
+ALTER TABLE compra ADD CONSTRAINT compra_pk PRIMARY KEY ( recluso_nif,
                                                           producto_id_producto );
 
 CREATE TABLE economato (
     carcel_cod_carcel INTEGER NOT NULL,
     hora_apertura     VARCHAR2(8),
-    hora_cierre       VARCHAR2(8),
-    cif               VARCHAR2(9) NOT NULL
+    hora_cierre       VARCHAR2(8)
 );
 
 ALTER TABLE economato ADD CONSTRAINT economato_pk PRIMARY KEY ( carcel_cod_carcel );
 
-ALTER TABLE economato ADD CONSTRAINT economato_cif_un UNIQUE ( cif );
-
 CREATE TABLE empleado (
-    nif                VARCHAR2(9) NOT NULL,
-    telefono           INTEGER NOT NULL,
-    euros_hora         FLOAT NOT NULL,
-    euros_horas_extras FLOAT NOT NULL,
-    euros_horas_noche  FLOAT NOT NULL,
-    carcel_cod_carcel  INTEGER NOT NULL,
-    director           VARCHAR2(9)
+    telefono             INTEGER NOT NULL,
+    euros_hora           FLOAT NOT NULL,
+    euros_horas_extras   FLOAT NOT NULL,
+    euros_horas_noche    FLOAT NOT NULL,
+    carcel_cod_carcel    INTEGER NOT NULL,
+    fecha_contratacion   DATE NOT NULL,
+    persona_nif          VARCHAR2(9) NOT NULL,
+    empleado_persona_nif VARCHAR2(9)
 );
 
-ALTER TABLE empleado ADD CONSTRAINT empleado_pk PRIMARY KEY ( nif );
+ALTER TABLE empleado ADD CONSTRAINT empleado_pk PRIMARY KEY ( persona_nif );
 
-CREATE TABLE empleado_recluso_voluntario (
-    nif VARCHAR2(9) NOT NULL
+CREATE TABLE empresa_externa (
+    cif              VARCHAR2(9) NOT NULL,
+    nombre           VARCHAR2(30) NOT NULL,
+    direccion_fisica VARCHAR2(30)
 );
 
-ALTER TABLE empleado_recluso_voluntario ADD CONSTRAINT empleado_recluso_voluntario_pk PRIMARY KEY ( nif );
+ALTER TABLE empresa_externa ADD CONSTRAINT empresa_externa_pk PRIMARY KEY ( cif );
+
+CREATE TABLE guardia (
+    empleado_persona_nif VARCHAR2(9) NOT NULL
+);
+
+ALTER TABLE guardia ADD CONSTRAINT guardia_pk PRIMARY KEY ( empleado_persona_nif );
 
 CREATE TABLE limpiador (
     nif VARCHAR2(9) NOT NULL
 );
 
 ALTER TABLE limpiador ADD CONSTRAINT limpiador_pk PRIMARY KEY ( nif );
+
+CREATE TABLE medico (
+    num_colegiado_medico INTEGER NOT NULL,
+    empleado_persona_nif VARCHAR2(9) NOT NULL
+);
+
+CREATE UNIQUE INDEX medico__idx ON
+    medico (
+        empleado_persona_nif
+    ASC );
+
+ALTER TABLE medico ADD CONSTRAINT medico_pk PRIMARY KEY ( num_colegiado_medico,
+                                                          empleado_persona_nif );
 
 CREATE TABLE modulo (
     id_modulo         INTEGER NOT NULL,
@@ -90,11 +110,12 @@ ALTER TABLE modulo
 ALTER TABLE modulo ADD CONSTRAINT modulo_pk PRIMARY KEY ( id_modulo );
 
 CREATE TABLE persona (
-    nif       VARCHAR2(9) NOT NULL,
-    num_ss    VARCHAR2(12),
-    nombre    VARCHAR2(30) NOT NULL,
-    apellidos VARCHAR2(60) NOT NULL,
-    email     VARCHAR2(30) NOT NULL
+    nif              VARCHAR2(9) NOT NULL,
+    num_ss           VARCHAR2(12),
+    nombre           VARCHAR2(30) NOT NULL,
+    apellidos        VARCHAR2(60) NOT NULL,
+    email            VARCHAR2(30) NOT NULL,
+    fecha_nacimiento DATE NOT NULL
 );
 
 ALTER TABLE persona ADD CONSTRAINT persona_pk PRIMARY KEY ( nif );
@@ -105,20 +126,21 @@ CREATE TABLE producto (
     id_producto                 INTEGER NOT NULL,
     precio                      FLOAT NOT NULL,
     nombre                      VARCHAR2(20) NOT NULL,
+    stock                       INTEGER NOT NULL,
     economato_carcel_cod_carcel INTEGER NOT NULL
 );
 
 ALTER TABLE producto ADD CONSTRAINT producto_pk PRIMARY KEY ( id_producto );
 
-CREATE TABLE recluso_permiso_zona (
-    reclusos_nif             VARCHAR2(9) NOT NULL,
-    zona_actividades_id_zona INTEGER NOT NULL
+CREATE TABLE provee_economato (
+    empresa_externa_cif  VARCHAR2(9) NOT NULL,
+    economato_cod_carcel INTEGER NOT NULL
 );
 
-ALTER TABLE recluso_permiso_zona ADD CONSTRAINT recluso_permiso_zona_pk PRIMARY KEY ( reclusos_nif,
-                                                                                      zona_actividades_id_zona );
+ALTER TABLE provee_economato ADD CONSTRAINT provee_economato_pk PRIMARY KEY ( empresa_externa_cif,
+                                                                              economato_cod_carcel );
 
-CREATE TABLE reclusos (
+CREATE TABLE recluso (
     nif                  VARCHAR2(9) NOT NULL,
     nis                  INTEGER NOT NULL,
     fecha_inicio_condena DATE NOT NULL,
@@ -126,26 +148,47 @@ CREATE TABLE reclusos (
     celda_id_celda       INTEGER NOT NULL
 );
 
-ALTER TABLE reclusos ADD CONSTRAINT reclusos_pk PRIMARY KEY ( nif );
+ALTER TABLE recluso ADD CONSTRAINT recluso_pk PRIMARY KEY ( nif );
 
-ALTER TABLE reclusos ADD CONSTRAINT reclusos_nis_un UNIQUE ( nis );
+ALTER TABLE recluso ADD CONSTRAINT recluso_nis_un UNIQUE ( nis );
 
-CREATE TABLE turnos (
-    id_turno     INTEGER NOT NULL,
-    entrada      DATE NOT NULL,
-    salida       DATE,
-    empleado_nif VARCHAR2(9) NOT NULL
+CREATE TABLE recluso_permiso_zona (
+    recluso_nif              VARCHAR2(9) NOT NULL,
+    zona_actividades_id_zona INTEGER NOT NULL
 );
 
-ALTER TABLE turnos ADD CONSTRAINT turnos_pk PRIMARY KEY ( id_turno );
+ALTER TABLE recluso_permiso_zona ADD CONSTRAINT recluso_permiso_zona_pk PRIMARY KEY ( recluso_nif,
+                                                                                      zona_actividades_id_zona );
+
+CREATE TABLE subcontrata (
+    empresa_externa_cif  VARCHAR2(9) NOT NULL,
+    empleado_persona_nif VARCHAR2(9) NOT NULL
+);
+
+ALTER TABLE subcontrata ADD CONSTRAINT subcontrata_pk PRIMARY KEY ( empleado_persona_nif );
+
+CREATE TABLE subcontrata_empleado (
+    nif VARCHAR2(9) NOT NULL
+);
+
+ALTER TABLE subcontrata_empleado ADD CONSTRAINT subcontrata_empleado_pk PRIMARY KEY ( nif );
+
+CREATE TABLE turno (
+    id_turno             INTEGER NOT NULL,
+    entrada              DATE NOT NULL,
+    salida               DATE,
+    empleado_persona_nif VARCHAR2(9) NOT NULL
+);
+
+ALTER TABLE turno ADD CONSTRAINT turno_pk PRIMARY KEY ( id_turno );
 
 CREATE TABLE visita (
-    reclusos_nif      VARCHAR2(9) NOT NULL,
+    recluso_nif       VARCHAR2(9) NOT NULL,
     visitante_nif     VARCHAR2(9) NOT NULL,
     fecha_hora_visita DATE NOT NULL
 );
 
-ALTER TABLE visita ADD CONSTRAINT visita_pk PRIMARY KEY ( reclusos_nif,
+ALTER TABLE visita ADD CONSTRAINT visita_pk PRIMARY KEY ( recluso_nif,
                                                           visitante_nif );
 
 CREATE TABLE visitante (
@@ -170,18 +213,18 @@ ALTER TABLE celda
     ADD CONSTRAINT celda_modulo_fk FOREIGN KEY ( modulo_id_modulo )
         REFERENCES modulo ( id_modulo );
 
---  ERROR: FK name length exceeds maximum allowed length(30) 
+
 ALTER TABLE cocinero
-    ADD CONSTRAINT cocinero_emp_rec_vol_fk FOREIGN KEY ( nif )
-        REFERENCES empleado_recluso_voluntario ( nif );
+    ADD CONSTRAINT cocinero_sub_empleado_fk FOREIGN KEY ( nif )
+        REFERENCES subcontrata_empleado ( nif );
 
 ALTER TABLE compra
     ADD CONSTRAINT compra_producto_fk FOREIGN KEY ( producto_id_producto )
         REFERENCES producto ( id_producto );
 
 ALTER TABLE compra
-    ADD CONSTRAINT compra_reclusos_fk FOREIGN KEY ( reclusos_nif )
-        REFERENCES reclusos ( nif );
+    ADD CONSTRAINT compra_recluso_fk FOREIGN KEY ( recluso_nif )
+        REFERENCES recluso ( nif );
 
 ALTER TABLE economato
     ADD CONSTRAINT economato_carcel_fk FOREIGN KEY ( carcel_cod_carcel )
@@ -192,17 +235,25 @@ ALTER TABLE empleado
         REFERENCES carcel ( cod_carcel );
 
 ALTER TABLE empleado
-    ADD CONSTRAINT empleado_empleado_fk FOREIGN KEY ( director )
-        REFERENCES empleado ( nif );
+    ADD CONSTRAINT empleado_empleado_fk FOREIGN KEY ( empleado_persona_nif )
+        REFERENCES empleado ( persona_nif );
 
 ALTER TABLE empleado
-    ADD CONSTRAINT empleado_persona_fk FOREIGN KEY ( nif )
+    ADD CONSTRAINT empleado_persona_fk FOREIGN KEY ( persona_nif )
         REFERENCES persona ( nif );
 
---  ERROR: FK name length exceeds maximum allowed length(30) 
+ALTER TABLE guardia
+    ADD CONSTRAINT guardia_empleado_fk FOREIGN KEY ( empleado_persona_nif )
+        REFERENCES empleado ( persona_nif );
+
+
 ALTER TABLE limpiador
-    ADD CONSTRAINT limpiador_emp_rec_vol_fk FOREIGN KEY ( nif )
-        REFERENCES empleado_recluso_voluntario ( nif );
+    ADD CONSTRAINT limpiador_sub_empleado_fk FOREIGN KEY ( nif )
+        REFERENCES subcontrata_empleado ( nif );
+
+ALTER TABLE medico
+    ADD CONSTRAINT medico_empleado_fk FOREIGN KEY ( empleado_persona_nif )
+        REFERENCES empleado ( persona_nif );
 
 ALTER TABLE modulo
     ADD CONSTRAINT modulo_carcel_fk FOREIGN KEY ( carcel_cod_carcel )
@@ -212,31 +263,48 @@ ALTER TABLE producto
     ADD CONSTRAINT producto_economato_fk FOREIGN KEY ( economato_carcel_cod_carcel )
         REFERENCES economato ( carcel_cod_carcel );
 
---  ERROR: FK name length exceeds maximum allowed length(30) 
-ALTER TABLE recluso_permiso_zona
-    ADD CONSTRAINT rec_permiso_zona_fk FOREIGN KEY ( reclusos_nif )
-        REFERENCES reclusos ( nif );
+ALTER TABLE provee_economato
+    ADD CONSTRAINT provee_economato_economato_fk FOREIGN KEY ( economato_cod_carcel )
+        REFERENCES economato ( carcel_cod_carcel );
 
---  ERROR: FK name length exceeds maximum allowed length(30) 
-ALTER TABLE recluso_permiso_zona
-    ADD CONSTRAINT permiso_zona_actividades_fk FOREIGN KEY ( zona_actividades_id_zona )
-        REFERENCES zona_actividades ( id_zona );
+ 
+ALTER TABLE provee_economato
+    ADD CONSTRAINT prove_econ_emp_externa_fk FOREIGN KEY ( empresa_externa_cif )
+        REFERENCES empresa_externa ( cif );
 
-ALTER TABLE reclusos
-    ADD CONSTRAINT reclusos_celda_fk FOREIGN KEY ( celda_id_celda )
+ALTER TABLE recluso
+    ADD CONSTRAINT recluso_celda_fk FOREIGN KEY ( celda_id_celda )
         REFERENCES celda ( id_celda );
 
-ALTER TABLE reclusos
-    ADD CONSTRAINT reclusos_persona_fk FOREIGN KEY ( nif )
+ 
+ALTER TABLE recluso_permiso_zona
+    ADD CONSTRAINT recl_perm_zona_recl_fk FOREIGN KEY ( recluso_nif )
+        REFERENCES recluso ( nif );
+
+ 
+ALTER TABLE recluso_permiso_zona
+    ADD CONSTRAINT recl_perm_zona_activ_fk FOREIGN KEY ( zona_actividades_id_zona )
+        REFERENCES zona_actividades ( id_zona );
+
+ALTER TABLE recluso
+    ADD CONSTRAINT recluso_persona_fk FOREIGN KEY ( nif )
         REFERENCES persona ( nif );
 
-ALTER TABLE turnos
-    ADD CONSTRAINT turnos_empleado_fk FOREIGN KEY ( empleado_nif )
-        REFERENCES empleado ( nif );
+ALTER TABLE subcontrata
+    ADD CONSTRAINT subcontrata_empleado_fk FOREIGN KEY ( empleado_persona_nif )
+        REFERENCES empleado ( persona_nif );
+
+ALTER TABLE subcontrata
+    ADD CONSTRAINT subcontrata_empresa_externa_fk FOREIGN KEY ( empresa_externa_cif )
+        REFERENCES empresa_externa ( cif );
+
+ALTER TABLE turno
+    ADD CONSTRAINT turno_empleado_fk FOREIGN KEY ( empleado_persona_nif )
+        REFERENCES empleado ( persona_nif );
 
 ALTER TABLE visita
-    ADD CONSTRAINT visita_reclusos_fk FOREIGN KEY ( reclusos_nif )
-        REFERENCES reclusos ( nif );
+    ADD CONSTRAINT visita_recluso_fk FOREIGN KEY ( recluso_nif )
+        REFERENCES recluso ( nif );
 
 ALTER TABLE visita
     ADD CONSTRAINT visita_visitante_fk FOREIGN KEY ( visitante_nif )
@@ -250,23 +318,15 @@ ALTER TABLE zona_actividades
     ADD CONSTRAINT zona_actividades_carcel_fk FOREIGN KEY ( carcel_cod_carcel )
         REFERENCES carcel ( cod_carcel );
 
---  ERROR: No Discriminator Column found in Arc FKArc_4 - constraint trigger for Arc cannot be generated 
 
---  ERROR: No Discriminator Column found in Arc FKArc_4 - constraint trigger for Arc cannot be generated 
-
---  ERROR: No Discriminator Column found in Arc FKArc_4 - constraint trigger for Arc cannot be generated
-
---  ERROR: No Discriminator Column found in Arc FKArc_3 - constraint trigger for Arc cannot be generated 
-
---  ERROR: No Discriminator Column found in Arc FKArc_3 - constraint trigger for Arc cannot be generated
 
 
 
 -- Informe de Resumen de Oracle SQL Developer Data Modeler: 
 -- 
--- CREATE TABLE                            17
--- CREATE INDEX                             0
--- ALTER TABLE                             42
+-- CREATE TABLE                            22
+-- CREATE INDEX                             1
+-- ALTER TABLE                             52
 -- CREATE VIEW                              0
 -- ALTER VIEW                               0
 -- CREATE PACKAGE                           0
