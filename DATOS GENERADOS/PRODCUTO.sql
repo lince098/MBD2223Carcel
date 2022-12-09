7ae47fa5660842390f965fb157eb2b33c3b3e0e9
Cristian Torres <cristiantorr98@gmail.com>
2022-12-09 20:22:12 +0100

    Esquema ER actualizado a la espera de confirmación

---
+++ DATOS GENERADOS/PRODCUTO.sql
@@ -0,0 +1,5 @@
+insert into PRODCUTO (ID_PRODUCTO, NOMBRE, PRECIO, STOCK) values (1, 'Aurilia', 5.1, 73);
+insert into PRODCUTO (ID_PRODUCTO, NOMBRE, PRECIO, STOCK) values (2, 'Rikki', 13.09, 100);
+insert into PRODCUTO (ID_PRODUCTO, NOMBRE, PRECIO, STOCK) values (3, 'Jessee', 3.48, 86);
+insert into PRODCUTO (ID_PRODUCTO, NOMBRE, PRECIO, STOCK) values (4, 'Whitney', 19.94, 38);
+insert into PRODCUTO (ID_PRODUCTO, NOMBRE, PRECIO, STOCK) values (5, 'Hermina', 8.32, 57);
